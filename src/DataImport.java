a138d922417261f7c77320cf58d383ea7b69c000
Nirb8 <57153743+Nirb8@users.noreply.github.com>
2021-04-19 17:21:50 -0400

    Parse comments into JSON

--- src/DataImport.java
+++ src/DataImport.java
@@ -1,10 +1,40 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import com.sun.javafx.scene.paint.GradientUtils.Parser;
 
 public class DataImport {
 
 	public static void main(String[] args) {
-		String jsonString = "blah blah";
-		//JSONObject obj = new JSONObject(jsonString);
+		
+		
+		JSONParser psr = new JSONParser();
+		
+		try {
+			Object obj = psr.parse(new FileReader("input.json"));
+			
+			JSONArray comments = (JSONArray) obj;
+			
+			
+			
+			
+			
+			Iterator<JSONObject> i = comments.iterator();
+			
+			while(i.hasNext()) {
+				System.out.println(i.next());
+			}
+			
+		} catch (IOException | ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
