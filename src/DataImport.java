7bc5ce45a130477112c2b9e0a61072f166c7d545
Veronica Kleinschmidt <kleinsv@rose-hulman.edu>
2021-04-29 17:03:22 -0400

    Minor import change

--- src/DataImport.java
+++ src/DataImport.java
@@ -6,7 +6,7 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.commons.text.StringEscapeUtils;
 
 import java.util.regex.*;
 
