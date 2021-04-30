156bcdc6c97e49b04bfe22f4053671d64c7b49c3
Nirb8 <57153743+Nirb8@users.noreply.github.com>
2021-04-29 22:54:38 -0400

    added code from main to dataimport so it can access db

--- src/DataImport.java
+++ src/DataImport.java
@@ -1,11 +1,18 @@
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.Scanner;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
+import ytTimestampLibS2G5.AuthHandler;
+import ytTimestampLibS2G5.DatabaseConnectionHandler;
+import ytTimestampLibS2G5.TimestampService;
+import ytTimestampLibS2G5.VideoService;
+
 import org.apache.commons.text.StringEscapeUtils;
 
 import java.util.regex.*;
@@ -13,6 +20,24 @@ import java.util.regex.*;
 public class DataImport {
 
 	public static void main(String[] args) {
+		
+		//taken from 
+		DatabaseConnectionHandler dbHandler = new DatabaseConnectionHandler(
+				"titan.csse.rose-hulman.edu", "ytTimestampLib_S2G5");
+		AuthHandler authHandler = new AuthHandler(dbHandler);
+		VideoService videoService = new VideoService(dbHandler);
+		TimestampService timestampService = new TimestampService(dbHandler);
+
+        // "simple" loop to continually read from console and call a switch statement
+        Scanner s = new Scanner(System.in);
+        boolean runStatus = true;
+        
+		
+		
+		
+		
+		
+		
 		JSONParser psr = new JSONParser();
 
 		Pattern pat = Pattern.compile("<a href=\"https:\\/\\/www\\.youtube\\.com\\/watch\\?v=(.*)&amp;t=(.*)\">.*<\\/a>\\s(.*)");
